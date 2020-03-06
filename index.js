6b95cb9f751da82b8c825dfb8f757fd1545f06b3
Lanre <laniyan@hotmail.co.uk>
2020-03-06 15:26:33 +0000

    changing nav bar

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 //$("h1").css("color", "red");
-$('li').attr('tabindex','0').focus();
+// $('li').attr('tabindex','0').focus();
+// $(document).ready(function(){
+//
+//   $(window).scroll(function(){
+//
+//     if($(window).scrollTop()>300){
+//       $('navigation').addClass('red');
+//   }else{
+//     $('navigation').removeClass('red');
+//   })
+// });
+// });
+
+var nav = document.getElementById('nav');
+window.onscroll = function(){
+  if(window.pageYOffset >800) {
+    nav.style.background = "transparent";
+    nav.style.border = "solid ";
+    document.querySelector('.navigation__nav').style.color = "black";
+  }
+  else if (window.pageYOffset <800){
+    nav.style.background = "white";
+    nav.style.border = "none";
+  }
+}
